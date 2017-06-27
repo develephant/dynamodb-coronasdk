37f6084d4b7b39657edd17a724f4fb507d0a349c
develephant <code@develephant.com>
2017-06-27 14:16:41 -0500

    clean

---
+++ project/aws_auth.lua
@@ -0,0 +1,207 @@
+
+local os = require('os')
+local crypto = require('crypto')
+
+local m = {}
+
+function m:new( options )
+    local options = options or {}
+
+    self.opts =  
+    {
+        aws_key = nil,
+        aws_secret = nil,
+
+        aws_service = "iam",
+        aws_region = "us-east-1",
+
+        method = "GET",
+        host = "iam.amazonaws.com",
+        path = "/",
+        query = { },
+        payload = "",
+
+        content_type = "application/x-www-form-urlencoded; charset=utf-8",
+
+        headers = { },
+
+        log = nil
+    }
+
+    --merge options
+    for option, value in pairs( options ) do 
+        self.opts[option] = value
+    end
+
+    self.amz_date = nil
+    self.amz_timestamp = nil
+
+    self.query_str = nil
+    self.canonical_headers = nil
+    self.signed_headers = nil
+    self.signed_payload = nil
+    self.signed_canonical_request = nil
+    self.role = nil
+    self.string_to_sign = nil
+    self.signing_key = nil
+    self.signature = nil
+    self.authorization = nil
+
+end
+
+function m:log( ... )
+    if self.opts.log then
+        print( ... )
+    end
+end
+
+function m:hmac(data, key, raw)
+    return crypto.hmac( crypto.sha256, data, key, raw )
+end
+
+function m:digest( data )
+    return crypto.digest( crypto.sha256, data )
+end
+
+function m:gen_date_and_timestamp()
+    local gmt_time = os.time(os.date('*t'))
+    self.amz_date = os.date('!%Y%m%d', gmt_time)
+    self.amz_timestamp = os.date('!%Y%m%dT%H%M%SZ', gmt_time)
+end
+
+function m:gen_default_headers()
+    self.opts.headers["Content-Type"] = self.opts.content_type
+    self.opts.headers["Host"] = self.opts.host
+    self.opts.headers["X-Amz-Date"] = self.amz_timestamp
+end
+
+function m:gen_query_string()
+    local query = self.opts.query
+    local query_tbl = {}
+    for param, value in pairs( query ) do 
+        table.insert(query_tbl, param..'='..value)
+    end
+
+    self.query_str = table.concat( query_tbl, '&' ) or ''
+
+    self:log( self.query_str )
+end
+
+function m:gen_canonical_headers()
+    local headers = self.opts.headers
+
+    local canonical_headers = {}
+    local signed_headers = {}
+
+    for header, value in pairs( headers ) do 
+        table.insert(canonical_headers, string.lower(header)..':'..value)
+        table.insert(signed_headers, string.lower(header))
+    end
+
+    table.sort( canonical_headers )
+    table.sort( signed_headers )
+
+    self.canonical_headers = table.concat( canonical_headers, '\n' )
+
+    self.signed_headers = table.concat( signed_headers, ';' )
+
+    self:log( self.canonical_headers )
+    self:log( self.signed_headers )
+end
+
+function m:gen_signed_payload()
+    self.signed_payload = string.lower( self:digest( self.opts.payload ) )
+    self:log( self.signed_payload )
+end
+
+function m:gen_canonical_request()
+    local request = 
+    {
+        self.opts.method,
+        self.opts.path,
+        self.query_str,
+        self.canonical_headers,
+        '', --clean line (required)
+        self.signed_headers,
+        self.signed_payload
+    }
+
+    local canonical_request = table.concat( request, '\n' )
+
+    self.signed_canonical_request = string.lower( self:digest( canonical_request ) )
+
+    self:log( self.signed_canonical_request )
+
+end
+
+function m:gen_role()
+    local role = 
+    {
+        self.amz_date,
+        self.opts.aws_region,
+        self.opts.aws_service,
+        'aws4_request'
+    }
+
+    self.role = table.concat( role, '/' )
+
+    self:log( self.role )
+end
+
+function m:gen_string_to_sign()
+
+    local string_to_sign = 
+    {
+        "AWS4-HMAC-SHA256",
+        self.amz_timestamp,
+        self.role,
+        self.signed_canonical_request
+    }
+
+    self.string_to_sign = table.concat( string_to_sign, '\n' )
+
+    self:log( self.string_to_sign )
+end
+
+function m:gen_signing_key()
+    local secret = 'AWS4'..self.opts.aws_secret
+
+    local key = self:hmac( self.amz_date, secret, true )
+    key = self:hmac( self.opts.aws_region, key, true )
+    key = self:hmac( self.opts.aws_service, key, true )
+    key = self:hmac( 'aws4_request', key, true )
+
+    self.signing_key = key --binary
+end
+
+function m:gen_signature()
+    self.signature = self:hmac( self.string_to_sign, self.signing_key )
+    self:log( self.signature )
+end
+
+function m:gen_auth_header()
+    self:gen_date_and_timestamp()
+    self:gen_default_headers()
+    self:gen_query_string()
+    self:gen_canonical_headers()
+    self:gen_signed_payload()
+    self:gen_canonical_request()
+    self:gen_role()
+    self:gen_string_to_sign()
+    self:gen_signing_key()
+    self:gen_signature()
+
+    self.authorization = 'AWS4-HMAC-SHA256 Credential='..self.opts.aws_key..'/'..self.role..' SignedHeaders='..self.signed_headers..', Signature='..self.signature
+
+    self:log( self.authorization )
+end
+
+function m:getHeaders()
+    self:gen_auth_header()
+    local headers = self.opts.headers
+    headers["Authorization"] = self.authorization
+    table.sort( headers )
+    return headers
+end
+
+return m
---
+++ project/aws_dynamodb.lua
@@ -0,0 +1,235 @@
+local json = require('json')
+local timer = require('timer')
+local mime = require('socket').mime
+
+local events = system.newEventDispatcher()
+
+local AWSAuth = require('aws_auth')
+
+local m = {}
+
+function m:new( options )
+
+  self.events = events
+
+  self.options = options
+
+  self.auth_opts = 
+  {
+    aws_key = options.aws_key,
+    aws_secret = options.aws_secret,
+
+    aws_service = 'dynamodb',
+    aws_region = options.aws_region,
+
+    host = 'dynamodb.'..options.aws_region..'.amazonaws.com',
+    method = "POST",
+
+    content_type = "application/x-amz-json-1.0; charset=UTF-8",
+    headers = { },
+
+    payload = "{}", --json
+
+    log = options.log
+  }
+
+  self.request_url = nil
+  self.request_params = nil
+  self.request_id = nil
+
+  self.last_error = nil
+
+  self.retry_timer = nil
+  self.retry_delay = 50 --delay in ms
+end
+
+function m:tryRequest()
+  local function listener( evt )
+    if (evt.isError) then
+      self.events:dispatchEvent({name="DynamoDbEvent", error=1, reason=evt.response, status=evt.status})
+    else
+      local response_tbl = json.decode(evt.response)
+      if evt.status == 200 then
+        self:clearRetryTimer()
+        if self.options.debug then
+          self:printTable(response_tbl)
+        end
+        self.events:dispatchEvent({name="DynamoDbEvent", error=nil, result=response_tbl, response=evt.response})
+      elseif evt.status == 400 then
+        --retry on ThrottlingException and ProvisionedThroughputExceededException
+        if string.ends(response_tbl.__type, "#ThrottlingException") then
+          self.events:dispatchEvent({name="DynamoDbEvent", error=1, reason='ThrottlingException, retrying...', status=evt.status})
+          self:retryRequest()
+        elseif string.ends(response_tbl.__type, "#ProvisionedThroughputExceededException") then
+          self.events:dispatchEvent({name="DynamoDbEvent", error=1, reason='ProvisionedThroughputExceededException, retrying...', status=evt.status})
+          self:retryRequest()
+        else --catch all other 400's
+          self:clearRetryTimer()
+          self.events:dispatchEvent({name="DynamoDbEvent", error=1, reason=response_tbl.message, status=evt.status}) 
+        end    
+      elseif evt.status == 500 then --internal server error
+        self.events:dispatchEvent({name="DynamoDbEvent", error=1, reason='Internal Server Error, retrying...', status=evt.status})
+        self:retryRequest()
+      elseif evt.status == 503 then --service unavailable
+        self.events:dispatchEvent({name="DynamoDbEvent", error=1, reason='Service Unavailable, retrying...', status=evt.status})
+        self:retryRequest()
+      else
+        --catch all error
+        self:clearRequest()
+        self:clearRetryTimer()
+        self.events:dispatchEvent({name="DynamoDbEvent", error=1, reason=response_tbl.message, status=evt.status})
+      end
+    end
+  end
+  
+  --send request
+  self.request_id = network.request(self.request_url, self.auth_opts.method, listener, self.request_params)
+end
+
+function m:retryRequest()
+  self.retry_timer = timer.performWithDelay(self.retry_delay, function() self:tryRequest(); end)
+  --bump retry delay for exponential backoff, up to a minute
+  self.retry_delay = self.retry_delay * 2
+  if self.retry_delay >= 30000 then
+    --cannot get request through
+    self:clearRequest()
+    self.events:dispatchEvent({name="DynamoDbEvent", isError=1, reason="Maximum retries exceeded. Failed after 30 seconds."})
+  else
+    self:tryRequest()
+  end
+end
+
+function m:clearRetryTimer()
+  if self.retry_timer then
+    timer.cancel(self.retry_timer)
+    self.retry_timer = nil
+    self.retry_delay = 50 --reset
+  end
+end
+
+function m:clearRequest()
+  network.cancel(self.request_id)
+  self.request_id = nil
+  self:clearRetryTimer()
+end
+
+function m:request(action, payload_tbl, listener)
+
+  --auth 
+  self.auth_opts.headers = 
+  {
+    ["X-Amz-Target"] = "DynamoDB_20120810."..action
+  }
+  self.auth_opts.payload = json.encode( payload_tbl )
+
+  AWSAuth:new( self.auth_opts )
+
+  local request_headers = AWSAuth:getHeaders()
+  request_headers["Accept-Encoding"] = "application/json"
+  request_headers["Content-Length"] = #self.auth_opts.payload
+
+  local params = 
+  {
+    headers = request_headers,
+    body = self.auth_opts.payload
+  }
+
+  self.request_params = params
+  self.request_url = 'https://'..self.auth_opts.host..'/'
+
+  self:tryRequest()
+
+end
+
+--data type helpers
+function m:S(string_val)
+  return {S = string_val}
+end
+
+function m:N(number_val, raw)
+
+  if not raw then
+    number_val = tostring( number_val )
+  end
+
+  return {N = number_val}
+end
+
+function m:B(binary_val)
+  return {B = mime.encode(binary_val)}
+end
+
+function m:BOOL(bool_val)
+  return {BOOL = bool_val}
+end
+
+function m:NULL()
+  return {NULL = true}
+end
+
+function m:M(map_tbl)
+  return {M = map_tbl}
+end
+
+function m:L(list_tbl)
+  return {L = list_tbl}
+end
+
+function m:SS(string_set)
+  return {SS = string_set}
+end
+
+function m:NS(number_set, raw)
+  local number_set_encoded = {}
+
+  for idx, v in ipairs( number_set ) do 
+    if not raw then 
+      table.insert(number_set_encoded, v)
+    else
+      table.insert(number_set_encoded, tostring( v ))
+    end
+  end
+
+  return {NS = number_set_encoded}
+end
+
+function m:BS(binary_set)
+  local binary_set_encoded = {}
+  --base64 encode binary values
+  for idx, v in ipairs( binary_set ) do 
+    table.insert(binary_set_encoded, mime.encode( v ))
+  end
+
+  return {BS = binary_set_encoded}
+end 
+
+-- @tparam table t The table data to print.
+-- @param[opt=""] indent Indentation char.
+function m:printTable( t, indent )
+  --== Print contents of a table, with keys sorted.
+  local names = {}
+  if not indent then indent = "" end
+  for n,g in pairs(t) do
+      table.insert(names,n)
+  end
+  table.sort(names)
+  for i,n in pairs(names) do
+      local v = t[n]
+      if type(v) == "table" then
+          if(v==t) then -- prevent endless loop if table contains reference to itself
+              print(indent..tostring(n)..": <-")
+          else
+              print(indent..tostring(n)..":")
+              self:printTable(v,indent.."   ")
+          end
+      else
+          if type(v) == "function" then
+              print(indent..tostring(n).."()")
+          else
+              print(indent..tostring(n)..": "..tostring(v))
+          end
+      end
+  end
+end
+
+return m
